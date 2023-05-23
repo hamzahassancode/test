6f8f4da8989fdf10391d15bbe9a36ac048dd3398
hamzahassancode <hamzahassancode@gmail.com>
2023-05-23 16:36:20 +0300

    first commit

---
+++ src/Circle2D.java
@@ -0,0 +1,26 @@
+public class Circle2D {
+    private final Point2D center;
+    private final double radius;
+
+    public Circle2D(Point2D center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public Point2D getCenter() {
+        return center;
+    }
+
+    public boolean overlaps(Circle2D other) {
+        double d = Math.sqrt(Math.pow(center.getX() - other.getCenter().getX(), 2) +
+                Math.pow(center.getY() - other.getCenter().getY(), 2));
+
+        return d < (radius + other.getRadius());
+    }
+
+
+}
